74a06c62c2e019249067ce33ec0ea49f0aca1e54
MaiRat <maik.ratzmer@gmail.com>
2026-02-23 20:53:24 +0100

    * cleanup/bugfixes

---
+++ HTML-Renderer-1.5.2/Source/HtmlRenderer/Core/Dom/CssBoxHelper.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using TheArtOfDev.HtmlRenderer.Adapters.Entities;
+using TheArtOfDev.HtmlRenderer.Core.Utils;
+
+namespace TheArtOfDev.HtmlRenderer.Core.Dom;
+
+internal static class CssBoxHelper
+{
+    public static CssBox CreateBox(HtmlTag tag, CssBox parent = null)
+    {
+        ArgChecker.AssertArgNotNull(tag, "tag");
+
+        if (tag.Name == HtmlConstants.Img)
+        {
+            return new CssBoxImage(parent, tag);
+        }
+        else if (tag.Name == HtmlConstants.Iframe)
+        {
+            return new CssBox(parent, tag);
+        }
+        else if (tag.Name == HtmlConstants.Hr)
+        {
+            return new CssBoxHr(parent, tag);
+        }
+        else
+        {
+            return new CssBox(parent, tag);
+        }
+    }
+
+    public static CssBox CreateBox(CssBox parent, HtmlTag tag = null, CssBox before = null)
+    {
+        ArgChecker.AssertArgNotNull(parent, "parent");
+
+        var newBox = new CssBox(parent, tag);
+        newBox.InheritStyle();
+
+        if (before != null)
+            newBox.SetBeforeBox(before);
+
+        return newBox;
+    }
+
+    public static CssBox CreateBlock() => new(null, null) { Display = CssConstants.Block };
+
+    public static CssBox CreateBlock(CssBox parent, HtmlTag tag = null, CssBox before = null)
+    {
+        ArgChecker.AssertArgNotNull(parent, "parent");
+
+        var newBox = CreateBox(parent, tag, before);
+        newBox.Display = CssConstants.Block;
+
+        return newBox;
+    }
+
+    internal static CssRect FirstWordOccourence(CssBox b, CssLineBox line)
+    {
+        if (b.Words.Count == 0 && b.Boxes.Count == 0)
+            return null;
+
+        if (b.Words.Count > 0)
+        {
+            foreach (CssRect word in b.Words)
+            {
+                if (line.Words.Contains(word))
+                    return word;
+            }
+
+            return null;
+        }
+        else
+        {
+            foreach (CssBox bb in b.Boxes)
+            {
+                CssRect w = FirstWordOccourence(bb, line);
+
+                if (w != null)
+                    return w;
+            }
+
+            return null;
+        }
+    }
+
+    public static void GetMinimumWidth_LongestWord(CssBox box, ref double maxWidth, ref CssRect maxWidthWord)
+    {
+        if (box.Words.Count > 0)
+        {
+            foreach (CssRect cssRect in box.Words)
+            {
+                if (cssRect.Width > maxWidth)
+                {
+                    maxWidth = cssRect.Width;
+                    maxWidthWord = cssRect;
+                }
+            }
+        }
+        else
+        {
+            foreach (CssBox childBox in box.Boxes)
+                GetMinimumWidth_LongestWord(childBox, ref maxWidth, ref maxWidthWord);
+        }
+    }
+
+    public static double GetWidthMarginDeep(CssBox box)
+    {
+        double sum = 0f;
+
+        if (box.Size.Width > 90999 || (box.ParentBox != null && box.ParentBox.Size.Width > 90999))
+        {
+            while (box != null)
+            {
+                sum += box.ActualMarginLeft + box.ActualMarginRight;
+                box = box.ParentBox;
+            }
+        }
+
+        return sum;
+    }
+
+    internal static double GetMaximumBottom(CssBox startBox, double currentMaxBottom)
+    {
+        foreach (var line in startBox.Rectangles.Keys)
+            currentMaxBottom = Math.Max(currentMaxBottom, startBox.Rectangles[line].Bottom);
+
+        foreach (var b in startBox.Boxes)
+            currentMaxBottom = Math.Max(currentMaxBottom, GetMaximumBottom(b, currentMaxBottom));
+
+        return currentMaxBottom;
+    }
+
+    public static void GetMinMaxSumWords(CssBox box, ref double min, ref double maxSum, ref double paddingSum, ref double marginSum)
+    {
+        double? oldSum = null;
+
+        // not inline (block) boxes start a new line so we need to reset the max sum
+        if (box.Display != CssConstants.Inline && box.Display != CssConstants.TableCell && box.WhiteSpace != CssConstants.NoWrap)
+        {
+            oldSum = maxSum;
+            maxSum = marginSum;
+        }
+
+        // add the padding 
+        paddingSum += box.ActualBorderLeftWidth + box.ActualBorderRightWidth + box.ActualPaddingRight + box.ActualPaddingLeft;
+
+
+        // for tables the padding also contains the spacing between cells
+        if (box.Display == CssConstants.Table)
+            paddingSum += CssLayoutEngineTable.GetTableSpacing(box);
+
+        if (box.Words.Count > 0)
+        {
+            // calculate the min and max sum for all the words in the box
+            foreach (CssRect word in box.Words)
+            {
+                maxSum += word.FullWidth + (word.HasSpaceBefore ? word.OwnerBox.ActualWordSpacing : 0);
+                min = Math.Max(min, word.Width);
+            }
+
+            // remove the last word padding
+            if (box.Words.Count > 0 && !box.Words[box.Words.Count - 1].HasSpaceAfter)
+                maxSum -= box.Words[box.Words.Count - 1].ActualWordSpacing;
+        }
+        else
+        {
+            // recursively on all the child boxes
+            for (int i = 0; i < box.Boxes.Count; i++)
+            {
+                CssBox childBox = box.Boxes[i];
+                marginSum += childBox.ActualMarginLeft + childBox.ActualMarginRight;
+
+                //maxSum += childBox.ActualMarginLeft + childBox.ActualMarginRight;
+                GetMinMaxSumWords(childBox, ref min, ref maxSum, ref paddingSum, ref marginSum);
+
+                marginSum -= childBox.ActualMarginLeft + childBox.ActualMarginRight;
+            }
+        }
+
+        // max sum is max of all the lines in the box
+        if (oldSum.HasValue)
+            maxSum = Math.Max(maxSum, oldSum.Value);
+    }
+
+    public static double GetMaxFloatBottom(CssBox box)
+    {
+        double maxBottom = 0;
+
+        if (box.ParentBox == null)
+            return maxBottom;
+
+        foreach (var sibling in box.ParentBox.Boxes)
+        {
+            if (sibling == box) break;
+            CollectMaxFloatBottom(sibling, ref maxBottom);
+        }
+
+        return maxBottom;
+    }
+
+    private static void CollectMaxFloatBottom(CssBox box, ref double maxBottom)
+    {
+        if (box.Float != CssConstants.None)
+            maxBottom = Math.Max(maxBottom, box.ActualBottom + box.ActualBorderBottomWidth);
+
+        foreach (var child in box.Boxes)
+            CollectMaxFloatBottom(child, ref maxBottom);
+    }
+
+    public static bool IsRectVisible(RRect rect, RRect clip)
+    {
+        rect.X -= 2;
+        rect.Width += 2;
+
+        clip.Intersect(rect);
+
+        if (clip != RRect.Empty)
+            return true;
+
+        return false;
+    }
+}
